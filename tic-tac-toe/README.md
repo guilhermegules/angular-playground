735751d90b84120dcca135819f345fcc850e4891
Guilherme Gules Moreira <guilherme.moreira@ilegra.com>
2020-04-12 22:06:38 -0300

    create app

---
+++ tic-tac-toe/README.md
@@ -0,0 +1,7 @@
+## Tic Tac Toe App 
+
+App build with angular and nebular theme;
+
+- Install all dependencies `npm install`;
+- Run application `npm start`;
+- Build application `npm run build`;
---
+++ tic-tac-toe/src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { SquareComponent } from './square/square.component';
+import { BoardComponent } from './board/board.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NbThemeModule, NbLayoutModule, NbButtonModule } from '@nebular/theme';
+import { NbEvaIconsModule } from '@nebular/eva-icons';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SquareComponent,
+    BoardComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    NbThemeModule.forRoot({ name: 'cosmic' }),
+    NbLayoutModule,
+    NbEvaIconsModule,
+    NbButtonModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
