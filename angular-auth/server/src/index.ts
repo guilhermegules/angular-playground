f44bd83167c6b876fcf93a74e1222bbc299a6d24
Guilherme Gules Moreira <guilherme.moreira@ilegra.com>
2020-05-01 12:09:46 -0300

    add: adding app server

---
+++ angular-auth/server/src/index.ts
@@ -0,0 +1,44 @@
+import * as dotenv from "dotenv";
+import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+
+dotenv.config();
+
+if (!process.env.PORT) {
+  process.exit(1);
+}
+
+const PORT: number = parseInt(process.env.PORT as string, 10);
+
+const app = express();
+
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+
+const server = app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
+
+type ModuleId = string | number;
+
+interface WebpackHotModule {
+  hot?: {
+    data: any;
+    accept(
+      dependencies: string[],
+      callback?: (updatedDependencies: ModuleId[]) => void
+    ): void;
+    accept(dependency: string, callback?: () => void): void;
+    accept(errHandler?: (err: Error) => void): void;
+    dispose(callback: (data: any) => void): void;
+  };
+}
+
+declare const module: WebpackHotModule;
+
+if (module.hot) {
+  module.hot.accept();
+  module.hot.dispose(() => server.close());
+}
