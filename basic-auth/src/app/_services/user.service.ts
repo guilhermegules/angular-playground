122b1da58f3b581332f9030e871b8ba14fc688bc
Guilherme Gules Moreira <guilherme.moreira@ilegra.com>
2020-05-03 19:24:35 -0300

    (feat): adding auth and user services

---
+++ basic-auth/src/app/_services/user.service.ts
@@ -0,0 +1,15 @@
+import { User } from '@app/_models';
+import { environment } from '@environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UserService {
+  constructor(private http: HttpClient) {}
+
+  getAll() {
+    return this.http.get<User[]>(`${environment.apiUrl}/users`);
+  }
+}
