0a05d5a493d8d8c1c4459082c4fb3e3e7e5b09d1
Guilherme Gules Moreira <guilherme.moreira@ilegra.com>
2020-05-03 19:25:06 -0300

    (feat): adding home and login components

---
+++ basic-auth/src/app/home/home.component.ts
@@ -0,0 +1,27 @@
+import { User } from '@app/_models';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from '@app/_services';
+import { first } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss'],
+})
+export class HomeComponent implements OnInit {
+  loading = false;
+  users: User[];
+
+  constructor(private userService: UserService) {}
+
+  ngOnInit(): void {
+    this.loading = true;
+    this.userService
+      .getAll()
+      .pipe(first())
+      .subscribe((users) => {
+        this.loading = false;
+        this.users = users;
+      });
+  }
+}
---
+++ basic-auth/src/app/login/login.component.ts
@@ -0,0 +1,63 @@
+import { first } from 'rxjs/operators';
+import { AuthenticationService } from '@app/_services';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss'],
+})
+export class LoginComponent implements OnInit {
+  loginForm: FormGroup;
+  loading = false;
+  submitted = false;
+  returnUrl: string;
+  error: string;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private route: ActivatedRoute,
+    private router: Router,
+    private authenticationService: AuthenticationService
+  ) {
+    if (this.authenticationService.userValue) {
+      this.router.navigate(['/']);
+    }
+  }
+
+  ngOnInit(): void {
+    this.loginForm = this.formBuilder.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required],
+    });
+    this.returnUrl = this.route.snapshot.queryParams[this.returnUrl] || '/';
+  }
+
+  get f() {
+    return this.loginForm.controls;
+  }
+
+  onSubmit() {
+    this.submitted = true;
+
+    if (this.loginForm.invalid) {
+      return;
+    }
+
+    this.loading = true;
+    this.authenticationService
+      .login(this.f.username.value, this.f.password.value)
+      .pipe(first())
+      .subscribe(
+        (data) => {
+          this.router.navigate([this.returnUrl]);
+        },
+        (error) => {
+          this.error = error;
+          this.loading = false;
+        }
+      );
+  }
+}
