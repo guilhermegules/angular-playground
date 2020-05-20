a88ad9f1167e99827448393b042a5a4576a3ca3a
Guilherme Gules Moreira <guilherme.moreira@ilegra.com>
2020-05-20 14:55:18 -0300

    removing project

--- movies-crud/src/app/shared/interfaces/alert.ts
+++
@@ -1,9 +0,0 @@
-export interface Alert {
-  title?: string;
-  description?: string;
-  btnSuccess?: string;
-  btnCancel?: string;
-  colorSuccess?: string;
-  colorCancel?: string;
-  btnClose?: string;
-}
