825eccffa3bdaeb03ae3551fc45b53ecdcbe28e2
Guilherme Gules <guilhermegules@gmail.com>
2022-07-12 22:18:17 -0300

    feat: creating a base form for abstract some default behaviour

---
+++ ng-forms/src/app/core/utils/base-form.component.spec.ts
@@ -0,0 +1,8 @@
+import { BaseFormDirective } from './base-form.component';
+
+describe('BaseFormDirective', () => {
+  it('should create an instance', () => {
+    const directive = new BaseFormDirective();
+    expect(directive).toBeTruthy();
+  });
+});
---
+++ ng-forms/src/app/core/utils/base-form.component.ts
@@ -0,0 +1,30 @@
+import { FormArray, FormGroup } from '@angular/forms';
+
+export abstract class BaseFormComponent {
+  public form!: FormGroup;
+
+  public abstract submit(): void;
+
+  public onSubmit(): void {
+    if (this.form.valid) {
+      this.submit();
+      return;
+    }
+
+    this.form.markAllAsTouched();
+  }
+
+  public checkValidityAndTouched(field: string): boolean {
+    return (
+      !!this.form.get(field)?.invalid &&
+      (!!this.form.get(field)?.touched || !!this.form.get(field)?.dirty)
+    );
+  }
+
+  public applyError(field: string): Record<string, boolean> {
+    return {
+      'has-error': this.checkValidityAndTouched(field),
+      'has-feedback': this.checkValidityAndTouched(field),
+    };
+  }
+}
